c45ee48a5e349a9081413b1eb0aef5aa1dfdfcaf
Sergey-Postolakiy <sergpostolaky@gmail.com>
2019-10-08 16:24:54 +0300

    f-1

--- test/js/script.js
+++ test/js/script.js
@@ -199,17 +199,18 @@ console.log(sum2([1], {}, '1', 1)); //3 */
 
 //развернуть массив и строки в нём
 let arr9 = ['abcd', 'ghjk'];
-let revArr = [];
+
 let revString = '';
 
 function rev(arrr) {
+   let revArr = [];
    for (let i = arrr.length - 1; i >= 0; i--) {
       let strin = arrr[i];
       for (let j = strin.length - 1; j >= 0; j--) {
          revString += strin[j];
          console.log(revString);
       }
-      revArr = arrr.push[i];
+      revArr = arrr.push[revString];
       console.log(revArr);
    }
    return revArr;
