ad02a51d296cfc3796e74395b042b2ce9f03545e
Sergey-Postolakiy <sergpostolaky@gmail.com>
2019-10-10 21:08:42 +0300

    f-4

--- test/js/script.js
+++ test/js/script.js
@@ -294,24 +294,22 @@ function well(x) {
    const good_count = x.filter(x => x == 'good').length;
    return good_count < 1 ? 'Fail!' :
       good_count < 3 ? 'Publish!' : 'I smell a series!';
-}
+}*/
 
-console.log(well(x));*/
-
-// function well(x) {
-//    let goodCount = 0;
-//    for (let i = 0; i < x.length; i++) {
-//       if (x[i] === 'good') {
-//          goodCount++;
-//       }
-//    }
-//    if (goodCount === 0) {
-//       return 'Fail!';
-//    } else if (goodCount <= 2) {
-//       return 'Publish!';
-//    } else if (goodCount >= 3) {
-//       return 'I smell a series!';
-//    }
-// }
+/*function well(x) {
+   let goodCount = 0;
+   for (let i = 0; i < x.length; i++) {
+      if (x[i] === 'good') {
+         goodCount++;
+      }
+   }
+   if (goodCount === 0) {
+      return 'Fail!';
+   } else if (goodCount <= 2) {
+      return 'Publish!';
+   } else if (goodCount >= 3) {
+      return 'I smell a series!';
+   }
+}*/
 
 console.log(well(x));
